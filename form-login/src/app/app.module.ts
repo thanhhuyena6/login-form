7b86dc21e8c0113b933a57bf4e9de6521131bd4a
Nguyen Thanh Huyen <huyen.nguyen@gumiviet.com>
2021-09-14 16:40:15 +0700

    feat: login

--- form-login/src/app/app.module.ts
+++ form-login/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from "@angular/forms";
+import {FormlyModule} from "@ngx-formly/core";
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({ extras: { lazyRender: true } }),
   ],
   providers: [],
   bootstrap: [AppComponent]
